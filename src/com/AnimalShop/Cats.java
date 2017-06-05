03576bf83b089d5e43662da8fd39e49d4a6cd439
MBaran88 <zdzibq@gmail.com>
2017-06-05 18:23:04 +0200

    Dodanie HashMapy z Kotami?

--- src/com/AnimalShop/Cats.java
+++ src/com/AnimalShop/Cats.java
@@ -1,16 +1,15 @@
 package com.AnimalShop;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by RENT on 2017-06-05.
  */
-public class Cats {
-
-    public class CatsMap<K,V> {
-
-        Map<K,V> CatList = new HashMap<K,V>();
-        
+public class Cats extends Main{
+    private HashMap<Integer,String> CatList = new HashMap<Integer,String>();
+    public HashMap<Integer,String> getCatList(){
+        return CatList;
     }
 }
